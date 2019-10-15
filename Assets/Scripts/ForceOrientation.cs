c202d3baf834c8476c1540c255f0fee4a5021b09
kamlesh9070 <kamlesh9070@gmail.comm>
2019-10-15 09:17:30 +0530

    Transparent Material Issues

--- Assets/Scripts/ForceOrientation.cs
+++ Assets/Scripts/ForceOrientation.cs
@@ -6,13 +6,21 @@ public class ForceOrientation : MonoBehaviour
 {
 
     public bool iaLandscapeLeft = true;
+    public bool autoRotation = false;
     // Start is called before the first frame update
     void Start()
     {
-        if(iaLandscapeLeft)
-            Screen.orientation = ScreenOrientation.LandscapeLeft;
-        else
-            Screen.orientation = ScreenOrientation.Portrait;
+        if(autoRotation)
+        {
+            Screen.orientation = ScreenOrientation.AutoRotation;
+        } else
+        {
+            if (iaLandscapeLeft)
+                Screen.orientation = ScreenOrientation.LandscapeLeft;
+            else
+                Screen.orientation = ScreenOrientation.Portrait;
+        }
+
     }
 
     // Update is called once per frame
