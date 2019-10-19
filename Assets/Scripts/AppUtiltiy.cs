d7bffc56d9dc6f85ba216d33140f81902c632c75
kamlesh9070 <kamlesh9070@gmail.comm>
2019-10-19 16:36:03 +0530

    Youtube Playlist

--- Assets/Scripts/AppUtiltiy.cs
+++ Assets/Scripts/AppUtiltiy.cs
@@ -10,6 +10,11 @@ public class AppUtiltiy : MonoBehaviour
         Application.OpenURL("http://tiny.cc/AY-podcast");
     }
 
+    public void OpenPodcastYoutubePlaylist()
+    {
+        Application.OpenURL("https://www.youtube.com/playlist?list=PLodZ8kFsDGFX5kco3t9xqYa1CHj72mL1E");
+    }
+
     public void FeedbackMail()
     {
         Application.OpenURL("mailto:gncapps@googlegroups.com?subject=Feedback/Bug%20Report%20of%20Akram%20Youth%20AR");
