3aa3e17ef4ad43a0739cebe39c7ae4a7ffdcd23c
laxitp <laxitp@cybage.com>
2019-11-19 14:04:03 +0530

    Check version on launch app,
    Developed in unity and check in unity, need to check in android
    Need to change model popup for message

---
+++ Assets/Scripts/StarUpSC/Startup.cs
@@ -0,0 +1,51 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Startup : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
+using UnityEngine;
+using UnityEditor;
+using System;
+[InitializeOnLoad]
+public class Startup
+{
+    static Startup()
+    {
+        try
+        {
+            ProcessCall processCall = new ProcessCall();
+            if (processCall.CheckVersion())
+            {
+                Debug.Log("Application Quit by fource!!");
+                ShowPopupExample.Init();
+                System.Threading.Thread.Sleep(30000);
+                Application.Quit();
+            }
+
+            // EditorApplication.update += Update;
+            //Debug.Log("Up and running app version: " + Application.version + " Unity version: " + Application.unityVersion);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex);
+        }
+    }
+
+    static void Update()
+    {
+        Debug.Log("Updating");
+    }
+}
---
+++ Assets/Scripts/StarUpSC/VersionProp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+//using JsonUtility;
+using System;
+
+//[Serializable]
+public class MyClass
+{
+    public int level;
+    public float timeElapsed;
+    public double version;
+}
+public class ProcessCall
+{
+    public ProcessCall()
+    {
+
+    }
+    public bool CheckVersion()
+    {
+        bool objRe = true;
+        Debug.Log("Call from Process Call");
+        using (WWW internetObj = new WWW("file:///C:/Users/laxitp/Desktop/version.json"))
+        {
+            if (!string.IsNullOrEmpty(internetObj.error))
+            {
+                Debug.Log(" Error: " + internetObj.error);
+            }
+            else
+            {
+                //Debug.Log(internetObj.text);
+                var vers = JsonUtility.FromJson<MyClass>(internetObj.text);
+                Debug.Log(JsonUtility.FromJson<MyClass>(internetObj.text).version);
+                if (Convert.ToDouble(Application.version) != vers.version)
+                {
+                    objRe = false;
+                }
+            }
+        }
+        return objRe;
+
+    }
+}
